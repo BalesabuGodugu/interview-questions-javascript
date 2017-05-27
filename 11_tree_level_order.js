04039d6f0d08d3334a759150ec9ab1daa5439125
Farbod Salimi <farbod_salimi@yahoo.com>
2017-05-27 15:59:21 -0700

    update

---
+++ 11_tree_level_order.js
@@ -0,0 +1,49 @@
+/*
+Given a tree, write a function that prints out the nodes of the tree in level order.
+
+tree : 
+        1
+      /   \
+     2     3
+    / \   / \
+   4   5 6   7
+   
+   traverse(tree) ->  1 2 3 4 5 6 7 
+*/
+
+class Node {
+	constructor (value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+function traverse (tree) {
+  if (!tree) return;
+  
+  let quque = [];
+  quque.push(tree);
+  
+  while (quque.length > 0) {
+    let curr = quque.shift();
+    console.log(curr.value);
+    
+    if (curr.left !== null) quque.push(curr.left);
+    if (curr.right !== null) quque.push(curr.right);
+  }
+}
+
+let left = new Node(2);
+left.left = new Node(4);
+left.right = new Node(5);
+
+let right = new Node(3);
+right.left = new Node(6);
+right.right = new Node(7);
+
+let tree = new Node(1);
+tree.left = left;
+tree.right = right;
+
+traverse(tree);
