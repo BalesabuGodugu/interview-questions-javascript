b3e5fbe1844a874ff9f8eaf0724e9f051c69132a
Farbod Salimi <farbod_salimi@yahoo.com>
2017-06-05 14:14:10 -0700

    update

---
+++ 36_bit_int_modulus.js
@@ -0,0 +1,13 @@
+/*
+Given a list of bytes a, each representing one byte of a larger 
+integer (ie. {0x12, 0x34, 0x56, 0x78} representing the integer 
+0x12345678), and an interger b, find a % b.
+
+mod({0x03, 0xED, 10}) => 5
+0x03ED
+
+*/
+
+function mod (a, b) {
+
+}
