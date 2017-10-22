119278b19085fa78048ce262d8cc0e57ed83d08e
Farbod Salimi <farbod_salimi@yahoo.com>
2017-10-21 22:52:57 -0700

    Minor

---
+++ 43_consecutive_array.js
@@ -0,0 +1,10 @@
+/*
+    Given an unsorted array, find the length of longest
+    sequence of consecutive numbers in the array.
+
+    eg.
+    consecutive([4, 2, 1, 6, 5])    => [4, 5, 6]
+    consecutive([5 ,5, 3, 1])       => [1], [3], or [5]
+
+*/
+
